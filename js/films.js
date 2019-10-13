55dae75bc14b1c42fb83000f6c601b7ad41bf39c
ilnur-ganeev00 <post4ilnur@gmail.com>
2019-10-13 14:42:11 +0300

    2.9.1-5-net

---
+++ js/films.js
@@ -0,0 +1,12 @@
+const genre_1 = 'фантастика'
+const genre_2 = 'боевик'
+const genre_3 = 'приключения'
+const genre_4 = 'фэнтези'
+const genre_5 = 'драма'
+const genre_6 = 'комедия'
+
+
+let genre_film_1 = genre_1 + ', ' + genre_2
+console.log('фильм 1:', genre_film_1)
+
+document.getElementById()
