4ae11a3968c1c461928f56049ceeeaed6a297420
ilnur-ganeev00 <post4ilnur@gmail.com>
2019-10-27 20:04:51 +0300

    4.4-1-array

--- js/films.js
+++ js/films.js
@@ -1,63 +1,30 @@
-const genre_1 = 'фантастика'
-const genre_2 = 'боевик'
-const genre_3 = 'приключения'
-const genre_4 = 'фэнтези'
-const genre_5 = 'драма'
-const genre_6 = 'комедия'
-const genre_7 = 'мультфильм'
+// Массивы
 
-let startFilm_1 = '10:00'
-let startFilm_2 = '12:00'
-let startFilm_3 = '13:00'
-let startFilm_4 = '14:00'
+let genres = ['фантастика', 'боевик', 'приключения', 'фэнтези', 'драма', 'комедия', 'мультфильм']
 
-let nameFilm_1 = 'Человек-паук: Вдали от дома'
-let nameFilm_2 = 'Собачья жизнь 2'
-let nameFilm_3 = 'История игрушек 4'
-let nameFilm_4 = 'Люди в черном: Интернэшнл'
+let films = [
+    ['10:00', 'Человек-паук: Вдали от дома', '1and2and3'],
+    ['12:00', 'Собачья жизнь 2', '4and5and6'],
+    ['14:00', 'История игрушек 4', '7and4and6'],
+    ['16:00', 'Люди в черном: Интернэшнл', '1and2and6'],
+]
 
-let genreFilm_1 = genre_1 + ', ' + genre_2 + ', ' + genre_3
-let genreFilm_2 = genre_4 + ', ' + genre_5 + ', ' + genre_6
-let genreFilm_3 = genre_7 + ', ' + genre_4 + ', ' + genre_6
-let genreFilm_4 = genre_1 + ', ' + genre_2 + ', ' + genre_6
+for (i=0; i < films.length; i++) {
+    let film = films[i]
+    t = i + 1
+    let elementStartFilm = document.getElementById('startFilm_' + t)
+    let elementNameFilm = document.getElementById('nameFilm_' + t)
+    let elementGenreFilm = document.getElementById('genreFilm_' + t)
 
-let elementStartFilm_1 = document.getElementById('startFilm_1')
-let elementNameFilm_1 = document.getElementById('nameFilm_1')
-let elementGenreFilm_1 = document.getElementById('genreFilm_1')
-
-elementStartFilm_1.innerHTML = startFilm_1
-elementNameFilm_1.innerHTML = nameFilm_1
-elementGenreFilm_1.innerHTML = genreFilm_1
-
-let elementStartFilm_2 = document.getElementById('startFilm_2')
-let elementNameFilm_2 = document.getElementById('nameFilm_2')
-let elementGenreFilm_2 = document.getElementById('genreFilm_2')
-
-elementStartFilm_2.innerHTML = startFilm_2
-elementNameFilm_2.innerHTML = nameFilm_2
-elementGenreFilm_2.innerHTML = genreFilm_2
-
-let elementStartFilm_3 = document.getElementById('startFilm_3')
-let elementNameFilm_3 = document.getElementById('nameFilm_3')
-let elementGenreFilm_3 = document.getElementById('genreFilm_3')
-
-elementStartFilm_3.innerHTML = startFilm_3
-elementNameFilm_3.innerHTML = nameFilm_3
-elementGenreFilm_3.innerHTML = genreFilm_3
-
-let elementStartFilm_4 = document.getElementById('startFilm_4')
-let elementNameFilm_4 = document.getElementById('nameFilm_4')
-let elementGenreFilm_4 = document.getElementById('genreFilm_4')
-
-elementStartFilm_4.innerHTML = startFilm_4
-elementNameFilm_4.innerHTML = nameFilm_4
-elementGenreFilm_4.innerHTML = genreFilm_4
-
-let elementNameFilmCarousel_1 = document.getElementById('nameFilmCarousel_1')
-elementNameFilmCarousel_1.innerHTML = nameFilm_1
-
-let elementNameFilmCarousel_2 = document.getElementById('nameFilmCarousel_2')
-elementNameFilmCarousel_2.innerHTML = nameFilm_2
-
-let elementNameFilmCarousel_4 = document.getElementById('nameFilmCarousel_4')
-elementNameFilmCarousel_4.innerHTML = nameFilm_4
+    for (m=0; m < film.length; m++) {
+        let matches = film[2].match(/(\d+)and(\d+)and(\d+)/);
+        let g_1 = Number(matches[1])-1;
+        let g_2 = Number(matches[2])-1;
+        let g_3 = Number(matches[3])-1;
+        let genreFilm = genres[g_1] + ', ' + genres[g_2] + ', ' + genres[g_3]
+        
+        elementStartFilm.innerHTML = film[0]
+        elementNameFilm.innerHTML = film[1]
+        elementGenreFilm.innerHTML = genreFilm
+    }
+}
---
+++ js/getPresent.js
@@ -0,0 +1,8 @@
+let nameGuest = document.getElementById('nameGuest')
+let buttonSendInfo = document.getElementById('sendPresentInfo')
+
+buttonSendInfo.onclick = function(){
+    console.log('Информация отправлена')
+}
+
+
