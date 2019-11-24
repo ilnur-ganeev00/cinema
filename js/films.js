015486023e86bff9054487954871d8222ed846b2
ilnur-ganeev00 <post4ilnur@gmail.com>
2019-11-24 17:59:07 +0300

    6.1-1-order form

--- js/films.js
+++ js/films.js
@@ -7,13 +7,13 @@ const mock = [
         genre: [0, 1, 2],
         filmHire: true,
         filmNew: true,
-        description:
+        description: 
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias, animi, commodi eius",
         image: "img/mov1.jpg", //для д.з. CA
         fb: "https://fb.com",
         twitter: "https://twitter.com",
         behance: "https://www.behance.net",
-        price: 200 
+        price: 100
     },
     {
         name: "Собачья жизнь 2",
@@ -21,13 +21,13 @@ const mock = [
         genre: [3, 4, 5],
         filmHire: true,
         filmNew: true,
-        description:
+        description: 
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias, animi, commodi eius",
         image: "img/mov2.jpg", //для д.з. CA
         fb: "https://fb.com",
         twitter: "https://twitter.com",
         behance: "https://www.behance.net",
-        price: 300
+        price: 100
     },
     {
         name: "История игрушек 4",
@@ -35,13 +35,13 @@ const mock = [
         genre: [6, 3, 5],
         filmHire: true,
         filmNew: false,
-        description:
+        description: 
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias, animi, commodi eius",
         image: "", //для д.з. CA
         fb: "https://fb.com",
         twitter: "https://twitter.com",
         behance: "https://www.behance.net",
-        price: 500
+        price: 100
     },
     {
         name: "Люди в чёрном: Интернэшнл",
@@ -49,13 +49,13 @@ const mock = [
         genre: [0, 1, 5],
         filmHire: true,
         filmNew: true,
-        description:
+        description: 
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias, animi, commodi eius",
         image: "img/mov3.jpg", //для д.з. CA
         fb: "https://fb.com",
         twitter: "https://twitter.com",
         behance: "https://www.behance.net",
-        price: 700
+        price: 100
     }
 ];
 
@@ -91,6 +91,104 @@ const genres = [
     }
 ];
 
+
+
+
+// Функции обработчики
+
+let order = function () {
+    // console.log('Функция order');
+
+    if (place.booking) {
+        alert('Место забронировано');
+    } else {
+        place.booking = 1;
+        console.log(place.booking);
+        // let el = EventTarget;
+        // console.log(el);
+        // seatBooked = el.seatNumber;
+        // console.log(seatBooked);
+
+    }
+};
+
+let placeToggle = function () {
+    console.log('Функция placeToggle')
+
+    // if (place.booking) {
+    //     // placeDiv.classList.add('placeBooked');
+    // } else {
+    //     placeDiv.classList.add('placeBooked');
+
+    //     // placeDiv.classList.add('placeFree');
+    // }
+
+};
+
+let placeContext = function () {
+    // console.log('Функция placeContext')
+    alert(place.price)
+};
+
+
+let placeHover = function () {
+    console.log('Функция placeHover')
+    // placeDiv.classList.add('placeHover')
+};
+let placeHoverOut = function () {
+    console.log('Функция placeHoverOut')
+};
+
+
+
+
+// Отображение мест в зале
+
+const places = [];
+
+// Задать количество мест в зале
+let seatsCount = 8;
+
+for (let y = 1; y < seatsCount + 1; y++) {
+    let bStatus = Math.round(Math.random());
+
+    place = {
+        seatNumber: y,
+        price: 100,
+        booking: bStatus,
+    };
+    places.push(place);
+};
+// console.log(places);
+
+
+let placesHTML = document.querySelector('.places');
+// let hall = document.createElement('div');
+
+// console.log(placesHTML);
+
+for (place of places) {
+    let placeDiv = document.createElement('div');
+    placeDiv.addEventListener('click', order);
+    placeDiv.addEventListener('click', placeToggle);
+    placeDiv.addEventListener('contextmenu', placeContext);
+    placeDiv.addEventListener('mouseover', placeHover);
+    placeDiv.addEventListener('mouseout', placeHoverOut)
+
+
+    placeDiv.innerHTML = place.seatNumber;
+    placeDiv.classList.add('placeDiv');
+
+    if (place.booking) {
+        placeDiv.classList.add('placeBooked');
+    } else {
+        placeDiv.classList.add('placeFree');
+    }
+
+    placesHTML.append(placeDiv);
+};
+
+
 //массивы для хранения отсортированных данных
 let filmsNew = [],
     filmsHire = [];
@@ -98,7 +196,7 @@ let filmsNew = [],
 //сортируем на новинки и в прокате
 for (let i = 0; i < mock.length; i++) {
     let currentFilm = mock[i];
-    
+
     if (currentFilm.filmHire) {
         filmsHire.push(currentFilm);
     }
@@ -110,15 +208,15 @@ for (let i = 0; i < mock.length; i++) {
 
 //объект-обертка для универсализации работы с данными
 const film = {
-    getName: function() {
+    getName: function () {
         return this.name;
     },
 
-    getStart: function() {
+    getStart: function () {
         return this.start;
     },
 
-    getGanre: function() {
+    getGanre: function () {
         //хранит текущие идентификаторы жанров. Здесь тоже используется this!
         const ganarsIds = this.genre;
 
@@ -135,7 +233,7 @@ const film = {
             //@see https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/find
             let genreText = genres.find(
                 //el содержит текущий элемент перебираемого массива genres
-                function(el) {
+                function (el) {
                     //если условие выполняется, то возвращается проверяемый элемент
                     return el.id == currentId;
                 }
@@ -151,98 +249,103 @@ const film = {
         return strGanars;
     },
 
+    renderFilmRow() {
 
-}
+        //добавлена строка вывода цены
+        let filmName = this.name,
+            filmStart = this.start,
+            filmGanars = film.getGanre.apply(this),
+            filmPrice = this.price;
 
+
+        let filmHTML = `
+            <td id="startFilm_${1}" class="movie-list__first-col">${filmStart}</td>
+            <td id="nameFilm_${1}" class="movie-list__second-col">${filmName}</td>
+            <td id="genreFilm_${1}" class="movie-list__third-col">${filmGanars}</td>
+            <td class="movie-list__fourth-col">${filmPrice}
+            </td>
+        `;
+        return filmHTML;
+    
+    
+    
+    
+    },
+
+    // renderFilmBlock()
+
+ 
+    //добавлен
+    getPrice: function () {
+        return this.price
+    }
+};
+
+//получаем DOM элемент с таблицей
+let tableDOM = document.getElementById("filmsHire");
 for (let i = 0; i < filmsHire.length; i++) {
-
     let currentFilm = filmsHire[i],
         filmName = film.getName.bind(currentFilm)(),
         filmStart = film.getStart.bind(currentFilm)(),
-        filmGanars = film.getGanre.bind(currentFilm)();
+        filmGanars = film.getGanre.bind(currentFilm)(),
+        filmRowHTML = film.renderFilmRow.bind(currentFilm)(),
+        tr = document.createElement("tr"), //содаем DOM элемент TR;
+        filmPrice = film.getPrice.bind(currentFilm)();
+    tr.innerHTML = filmRowHTML; //записываем в DOM элемент HTML разметку
 
-    console.log('Массив №', i);
-    console.log(filmName);
-    console.log(filmStart);
-    console.log(filmGanars);
+    //вешаем обработчик события на строку, вызывающий модальное окно
+    /*** РАЗОБРАТЬ */
+    tr.onclick = function () {
+        // 1. Находим элемент с формой заказ
+        // 2. Изменить состояние из display: none -> display: block;
+        // 3. Отобразить данные по бронированию фильма
 
-    let tableDOM = document.getElementById("filmsHire");
+        orderForm.style.display = 'block';
 
+        let orderFilmName = document.getElementById('orderFilmName'),
+            orderFilmStart = document.getElementById('orderFilmStart'),
+            orderFilmGanar = document.getElementById('orderFilmGanar'),
+            orderFilmPrice = document.getElementById('orderFilmPrice');
 
-    let filmHTML = `
-    <td id="startFilm_${i}" class="movie-list__first-col">${filmStart}
-    </td>
-    <td id="nameFilm_${i}" class="movie-list__second-col">
-        ${filmName}
-    </td>
-    <td id="genreFilm_${i}" class="movie-list__third-col">${filmGanars}
-    </td>
-    <td class="movie-list__fourth-col">
-        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-            width="33px" height="33px">
-            <path fill-rule="evenodd" fill="rgb(255, 255, 255)"
-                d="M16.505,32.994 C7.393,32.994 0.005,25.607 0.005,16.494 C0.005,7.381 7.393,-0.006 16.505,-0.006 C25.618,-0.006 33.005,7.381 33.005,16.494 C33.005,25.607 25.618,32.994 16.505,32.994 ZM24.214,14.591 L18.356,14.631 L18.356,8.796 L13.958,8.777 L13.958,14.637 L8.083,14.637 L8.083,19.074 L13.958,19.074 L13.958,24.889 L18.336,24.889 L18.307,19.074 L24.231,19.074 L24.214,14.591 Z" />
-        </svg>
-    </td>
-`;
- 
-    let tr = document.createElement("tr");
-    tr.innerHTML = filmHTML;
+        orderFilmName.innerHTML = filmName;
+        orderFilmStart.innerHTML = filmStart;
+        orderFilmGanar.innerHTML = filmGanars;
+        orderFilmPrice.innerHTML = filmPrice;
+    
+        let orderFilmCountTicket = document.getElementById('orderFilmCountTicket'),
+            orderFilmTotalPrice = document.getElementById('orderFilmTotalPrice');
+    
+        orderFilmTotalPrice.innerHTML = filmPrice * orderFilmCountTicket.value;
+    
+        orderFilmCountTicket.onchange = function () {
+          orderFilmTotalPrice.innerHTML = filmPrice * orderFilmCountTicket.value;
+        }
+    }
+
     tableDOM.appendChild(tr); //добавляем в DOM элемент таблицы DOM элемент строки с фильмом
+}
 
+// Закрытие модального окна
+/*** РАЗОБРАТЬ Event Handler */
+let orderForm = document.getElementById('orderForm');
+let closeOrderForm = document.getElementById('closeOrderForm');
+
+closeOrderForm.onclick = function () {
+    orderForm.style.display = 'none';
+}
+
+// Валидация ввода имени
+/** РАЗОБРАТЬ Event Handler */
+let sendOrder = document.getElementById('sendOrder');
+sendOrder.onclick = function () {
+  let orderClinetName = document.getElementById('orderClinetName');
+
+  if (orderClinetName.value) {
+    orderClinetName.style.border = '1px solid #bebebe';
+  } else {
+    orderClinetName.style.border = '2px solid red';
+  }
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////// Первоначальный вариант по теме массивы 
-
-// let genres = ['фантастика', 'боевик', 'приключения', 'фэнтези', 'драма', 'комедия', 'мультфильм'];
-
-// let films = [
-//     ['10:00', 'Человек-паук: Вдали от дома', '1and2and3'],
-//     ['12:00', 'Собачья жизнь 2', '4and5and6'],
-//     ['14:00', 'История игрушек 4', '7and4and6'],
-//     ['16:00', 'Люди в черном: Интернэшнл', '1and2and6'],
-// ]
-
-// for (let i = 0; i < films.length; i++) {
-//     let film = films[i];
-//     let elementStartFilm = document.getElementById('startFilm_' + (i + 1));
-//     let elementNameFilm = document.getElementById('nameFilm_' + (i + 1));
-//     let elementGenreFilm = document.getElementById('genreFilm_' + (i + 1));
-
-//     for (let m = 0; m < film.length; m++) {
-//         let matches = film[2].match(/(\d+)and(\d+)and(\d+)/);
-//         let g_1 = Number(matches[1]) - 1;
-//         let g_2 = Number(matches[2]) - 1;
-//         let g_3 = Number(matches[3]) - 1;
-//         let genreFilm = genres[g_1] + ', ' + genres[g_2] + ', ' + genres[g_3];
-
-//         let genreTest = genres.join(', ');
-//         console.log(genreTest);
-
-//         elementStartFilm.innerHTML = film[0];
-//         elementNameFilm.innerHTML = film[1];
-//         elementGenreFilm.innerHTML = genreFilm;
-//     }
-// }
