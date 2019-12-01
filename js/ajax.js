09cdef406fd096c58fa0419217c55950ba030380
ilnur-ganeev00 <post4ilnur@gmail.com>
2019-12-01 19:16:46 +0300

    7.3-1

---
+++ js/ajax.js
@@ -0,0 +1,65 @@
+const SYPEX_URL = 'https://api.sypexgeo.net',
+    CITIES_URL = 'https://glavpunkt.ru/api/get_rf_cities';
+
+let cities;
+
+/**
+ * 
+ * @param {*} api_url - адрес запроса
+ */
+
+function getRequest(api_url, callback) {
+    let xhr = new XMLHttpRequest(),
+        async = true;
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            callback.call(xhr.responseText);
+        }
+    }
+
+    xhr.open('GET', api_url, async);
+    xhr.send();
+    // return (xhr.responseText);
+};
+
+jQuery(($)=> {
+    $('#city_name').on('click', function(e){
+        e.preventDefault();
+        choose_city.style.display = 'block';
+
+        closeChoose_city.onclick = function () {
+            choose_city.style.display = 'none';
+        }
+
+        if (!cities) {
+            getRequest(CITIES_URL, function(){
+                // console.log(cities);
+                cities = $.parseJSON(this);
+            });
+        
+        }
+
+        $('body').on('input keyup', 'input[name=choose_city__form]', function() {
+            // console.log(cities);
+            let search = $(this).val(),
+                html = '<ul>';
+            for (let i = 0; i < cities.lenght; i++) {
+                if (cities[i].toLowerCase().indexOf(search.toLowerCase()) >= 0) {
+                    html += '<li>' + cities[i]['name'] + ' (' + cities[i]['area'] + ')</li>';
+                }
+            }
+            html += '</ul>';
+
+        })
+    });
+});
+
+jQuery(document).ready(($) => {
+    // console.log('start request');
+    getRequest(SYPEX_URL, function(){
+        $answer = $.parseJSON(this)
+        let city = $answer['city']['name_ru'];
+        // console.log(city);
+        $('#city_name').html(city);
+    });
+});
