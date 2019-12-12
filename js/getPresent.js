586ff108faa960d9df04d13728f3e5aa1280a394
ilnur-ganeev00 <post4ilnur@gmail.com>
2019-12-12 15:45:37 +0300

    3.3-2-for review

--- js/getPresent.js
+++ js/getPresent.js
@@ -5,4 +5,20 @@ buttonSendInfo.onclick = function(){
     console.log('Информация отправлена')
 }
 
+let getPresent = document.getElementById('getPresent'),
+    closeGetPresent = document.getElementById('closeGetPresent');
+
+closeGetPresent.onclick = function () {
+        getPresent.style.display = 'none';
+}
+
+
+let giftButton = document.getElementById('giftButton');
+
+giftButton.onclick = function () {
+
+    getPresent.style.display = 'block';
+
+
+}
 
