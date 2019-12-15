858772616bc8c2901879a07cdd4e11a43ec69518
ilnur-ganeev00 <post4ilnur@gmail.com>
2019-12-15 20:58:26 +0300

    3.7-2-upd

--- js/burger.js
+++ js/burger.js
@@ -2,15 +2,14 @@ let burger = document.getElementById('burger'),
 closeBurger = document.getElementById('closeBurger');
 
 closeBurger.onclick = function () {
-    burger.style.display = 'none';
     closeBurger.style.display = 'none';
-    
+    burger.classList.toggle('showBurgerNav');
 }
 
 
 let burgerButton = document.getElementById('burgerButton');
 
 burgerButton.onclick = function () {
-    burger.style.display = 'block';
     closeBurger.style.display = 'block';
+    burger.classList.add('showBurgerNav');
 }
